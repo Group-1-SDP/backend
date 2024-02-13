8d2e07d32d72833ac2fb885a89cf3e63a903e4ca
matth1eumartin <94123711+matth1eumartin@users.noreply.github.com>
2024-02-13 14:59:50 +0000

    Showing POST Request

--- server.py
+++ server.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def api():
     return "Hello, World!"
 
+@app.route('/testPost', methods=['POST'])
+def testPost():
+    data = request.get_json()
+    return jsonify(data)
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
