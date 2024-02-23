2de10ee1620c7154756640e9891e2debbf689552
Cem Yilmaz <cemyilmaz863@gmail.com>
2024-02-23 12:06:13 +0000

    Basic User Login + simple validation

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy  import SQLAlchemy
 from flask_cors import CORS, cross_origin
+from flask_bcrypt import Bcrypt
 from datetime import datetime
-from models import db, User
+from models import db, User, Task
 
 import re
 
@@ -10,6 +11,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tickbox.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #to supress warning
 CORS(app, resources={r"/api/*": {"origins": "*"}})
+bcrypt = Bcrypt(app)
 db.init_app(app)
 
 with app.app_context():
@@ -28,7 +30,8 @@ def register():
     email = data['email']
     username = data['username']
     password = data['password']
-    new_user = User(username=username, password=password)
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+    new_user = User(username=username, password=hashed_password, email=email)
 
     userAlreadyExists = User.query.filter_by(username=username).first()
     if userAlreadyExists:
@@ -41,6 +44,39 @@ def register():
     db.session.add(new_user)
     db.session.commit()
     return jsonify({'message': 'New user created!'})
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        return jsonify({'message': 'User does not exist!'}), 404
+    if not bcrypt.check_password_hash(user.password, password):
+        return jsonify({'message': 'Invalid password!'}), 401
+    else:
+        return jsonify({'message': 'Logged in successfully!'}), 200
+    
+
+@app.route('/api/addTask', methods=['POST'])
+def addTask():
+    return jsonify({'message': 'Not Implemented Yet!'}), 200
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
--- models.py
+++ models.py
@@ -11,5 +11,14 @@ def generateString():
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.String(5), default=generateString, primary_key=True)
-    username = db.Column(db.String(80), nullable=False)
+    username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+
+class Task(db.Model):
+    __tablename__ = 'tasks'
+    id = db.Column(db.String(10), primary_key=True)
+    task_id = db.Column(db.String(5), default=generateString, nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    completed = db.Column(db.Boolean, default=False, nullable=False)
+    contents = db.Column(db.String(200), nullable=False)
